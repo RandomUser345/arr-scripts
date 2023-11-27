b327bc3cdebedfeff656268893a4a8385b6ab8ce
Max Pfandl <maxanton.pfandl@s-itsolutions.at>
2023-11-27 09:21:51 +0100

    Update to my repo

--- lidarr/scripts_init.bash
+++ lidarr/scripts_init.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bash
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/lidarr/setup.bash | bash
+curl https://raw.githubusercontent.com/RandomUser345/arr-scripts/main/lidarr/setup.bash | bash
 exit
